45411775909543a160fa1d453bfc6009e9befe93
amir radman <amirradman@gmail.com>
2017-11-16 01:34:38 -0800

    Completed user functionality

--- GeneralUser.java
+++ GeneralUser.java
@@ -48,6 +48,9 @@ public class GeneralUser extends JFrame{
 		final JTextField uidReserve = new JTextField();
 		final JTextField fidReserve = new JTextField();
 		final JTextField aName = new JTextField();
+		final JTextField seatNum = new JTextField();
+		final JTextField uidCanceledReserve = new JTextField();
+		final JTextField fidCanceledReserve = new JTextField();
 	
 	
 	
@@ -56,7 +59,9 @@ public class GeneralUser extends JFrame{
 		JButton findFlight = new JButton("Find Flight");
 		JButton notReservedFlights = new JButton("Not Reserved Flights");
 		JButton findHighSeason = new JButton("Find High Season(s)");
-
+		JButton showUsersButton = new JButton("Show Current Users");
+		JButton flightswithseats = new JButton("Show flights with given seat number");
+		JButton cancelReservationButton = new JButton("Cancel Reservation");
 	
 	
 		JPanel northPanel = new JPanel();
@@ -71,22 +76,43 @@ public class GeneralUser extends JFrame{
 		northPanel.add(ageText);
 		northPanel.add(addUserButton);
 		
+		northPanel.add(new JLabel("                                                                  "));
+		
 		//Flight Reservation using uid and fid fields + button
 		northPanel.add(new JLabel ("User ID: "));
 		northPanel.add(uidReserve);
 		northPanel.add(new JLabel ("Flight ID: "));
 		northPanel.add(fidReserve);
 		northPanel.add(reserveButton);
-		
+		northPanel.add(new JLabel("                                                                  "));
+
 
 		//Find flights using airline Name fields + button
 		northPanel.add(new JLabel ("AirLine Name: "));
 		northPanel.add(aName);
 		northPanel.add(findFlight);
+		northPanel.add(new JLabel("                                                                  "));
+		
+		//Find flights using seat number
+		northPanel.add(new JLabel ("Number of seats"));
+		northPanel.add(seatNum);
+		northPanel.add(flightswithseats);
+		northPanel.add(new JLabel("                                                                  "));
+		
+		
+		//Cancel Reservation
+		northPanel.add(new JLabel ("User ID: "));
+		northPanel.add(uidCanceledReserve);
+		northPanel.add(new JLabel ("Flight ID: "));
+		northPanel.add(fidCanceledReserve);
+		northPanel.add(cancelReservationButton);
+		northPanel.add(new JLabel("                                                                  "));
+
 		
 		//Features that do not require user input-Return data statistics
 		southPanel.add(notReservedFlights);
 		southPanel.add(findHighSeason);
+		southPanel.add(showUsersButton);
 		
 		
 		add(northPanel, BorderLayout.NORTH);
@@ -122,16 +148,37 @@ public class GeneralUser extends JFrame{
 			}
 		});
 		
-		
-		
+		//Cancel Reservation
+		cancelReservationButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				String uid = uidCanceledReserve.getText();
+				String fid = fidCanceledReserve.getText();
+				PreparedStatement stmt = null;
+				try{
+					stmt = myConn.prepareStatement("insert into canceledReservation(uid,fid,canceledDate) values(?,?,current_Date())");
+					stmt.setString(1, uid);
+					stmt.setString(2, fid);
+					stmt.executeUpdate();
+				
+					uidCanceledReserve.setText("");
+					fidCanceledReserve.setText("");
+				}
+				catch(Exception exc)
+				{
+					exc.printStackTrace();
+				}
+				finally{
+					try{
+						stmt.close();
+					}
+					catch(SQLException exc){
+						exc.printStackTrace();
 
-//		{{CHOOSE A BUTTON}}.addActionListener(new ActionListener(){
-//			public void actionPerformed(ActionEvent arg0) {
-//				// CANCEL RESERVATION
-//			}
-//		});
+					}
+				}
+			}
+		});
 
-		
 		
 		// Actionlistener to make reservation
 		reserveButton.addActionListener(new ActionListener(){
@@ -172,7 +219,7 @@ public class GeneralUser extends JFrame{
 				try{
 					
 		            Choice choices = new Choice();
-		            JFrame frame = new JFrame("Search Result");
+		            JFrame frame = new JFrame("Locate flights with airline name");
 					stmt = myConn.prepareStatement("Select * from flightList where aName = ?");
 					stmt.setString(1, airlineName);
 					stmt.executeQuery();
@@ -212,7 +259,7 @@ public class GeneralUser extends JFrame{
 				try{
 					
 		            Choice choices = new Choice();
-		            JFrame frame = new JFrame("Search Result");
+		            JFrame frame = new JFrame("Flights with 0 reservations");
 					stmt = myConn.prepareStatement("select FL.fid,aName,numSeats,uid,reservedDate from flightList FL left Outer JOIN reservation R on FL.fid = R.fid;");
 					aName.setText("");
 					
@@ -243,14 +290,14 @@ public class GeneralUser extends JFrame{
 			}
 		});
 
-		//
+		// Find Months with reservations more than 3
 		findHighSeason.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				PreparedStatement stmt = null;
 				try{
 					
 		            Choice choices = new Choice();
-		            JFrame frame = new JFrame("Search Result");
+		            JFrame frame = new JFrame("High Season Month(s)");
 					stmt = myConn.prepareStatement("select distinct MonthName(reservedDate) as 'High Season Month(s)' from reservation group by Month(reservedDate) having(count(*)>=3)");
 					
 					aName.setText("");
@@ -278,16 +325,85 @@ public class GeneralUser extends JFrame{
 			}
 		});
 		
+		
+		//Show Current Users
+		showUsersButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				PreparedStatement stmt = null;
+		        try {
+		            Choice choices = new Choice();
+		             JFrame frame = new JFrame("Current Users");
+		             stmt =  myConn.prepareStatement("Select * from user");
+		             ResultSet rs = stmt.executeQuery();
+		        while (rs.next()) {
+		            String uid = rs.getString("uid");
+		            String uName = rs.getString("uName");
+		            String age = rs.getString("age");
+		            String Query = uid +" "+ uName + " "+age;
+		            choices.addItem(Query);
+		            frame.add(choices);
+		            frame.setSize(350, 150);
+		            frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
 
-//		
-//		
-//		{{CHOOSE A BUTTON}}.addActionListener(new ActionListener(){
-//			public void actionPerformed(ActionEvent arg0) {
-//				// FINDING FLIGHTS WITH ? # SEATS
-//			}
-//		});
-//		
-//	
+		        }
+
+		    } catch (SQLException e) {
+
+		    	e.printStackTrace();
+		    	}
+		        finally{
+		        	try {
+						stmt.close();
+					} catch (SQLException e2) {
+						// TODO: handle exception
+						e2.printStackTrace();
+					}
+		        }
+			}
+		});
+		
+		
+		//Find flights with certain # seats
+		flightswithseats.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				PreparedStatement stmt = null;
+				String seat = seatNum.getText();
+		        try {
+		            Choice choices = new Choice();
+		             JFrame frame = new JFrame("Flights with __ Seats");
+		             stmt =  myConn.prepareStatement("Select * from flightList where numSeats = ?");
+		             stmt.setString(1,seat);
+		             ResultSet rs = stmt.executeQuery();
+		        while (rs.next()) {
+		            String fid = rs.getString("fid");
+		            String airlineName = rs.getString("aName");
+		            String numSeat = rs.getString("numSeats");
+		            String Query = fid +" "+ airlineName + " "+numSeat;
+		            choices.addItem(Query);
+		            frame.add(choices);
+		            frame.setSize(350, 150);
+		            frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+
+		        }
+
+		    } catch (SQLException e) {
+
+		    	e.printStackTrace();
+		    	}
+		        finally{
+		        	try{
+		        		stmt.close();
+		        	}
+		        	catch(SQLException exc)
+		        	{
+		        		exc.printStackTrace();
+		        	}
+		        }
+			}
+		});
+		
 	
 	}
 	public static final int DEFAULT_WIDTH = 800;
